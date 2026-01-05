21603ce62d650c20833f517cdee7fb9551abd58c
Raul Marchan Catala <alountk@gmail.com>
2026-01-05 12:51:13 +0100

    feat: add initial App component and entry point

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Meta Advanced React Lab</h1>
+      <p>Welcome to the Advanced React course labs.</p>
+    </div>
+  );
+}
+
+export default App;
